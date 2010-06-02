29fc3dd31c473cb2e4c6f4407db9a0020e7a3204
Jaak Pruulmann-Vengerfeldt <jjpp@meso.ee>
2010-06-02 11:49:06 +0300

    varahommikune.

---
+++ gen-nouns.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+W=$1
+XFST=/home/jjpp/dev/keel/xerox/bin/xfst
+LOOKUP=/home/jjpp/dev/keel/xerox/bin/lookup
+FST=${FST:-eesti.fst}
+
+$XFST -e "load $FST" -e 'invert' -e "save reverse-$FST" -stop
+
+(
+for num in sg pl; do
+	for cs in nom gen part ill in el all ad abl tr ter es ab kom adit; do
+		echo "$W+S+$num+$cs"
+	done
+done) | $LOOKUP reverse-$FST 
+
+
+
